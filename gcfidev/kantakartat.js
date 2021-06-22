ede0ce1138844a1e863e3ac15b29aba2be12371d
georapo <33866355+georapo@users.noreply.github.com>
2021-06-22 13:05:51 +0300

    Update kantakartat.js

--- gcfidev/kantakartat.js
+++ gcfidev/kantakartat.js
@@ -52,7 +52,7 @@ function kantakarttaAvailable() {
     for(var i = 0; i < kantakartatDefs.length; i++) {
 		if (c[0] > kantakartatDefs[i][1] && c[0] < kantakartatDefs[i][3] && c[1] > kantakartatDefs[i][2] && c[1] < kantakartatDefs[i][4]) {
 		  
-		   if (pointinpolygontest([c[0],c[1]], kuntarajat1000k[i][1]) || i>kuntarajat1000k.length-1) {
+		   if (pointinpolygontest([c[0],c[1]], kuntarajat1000k[i][1]) || (i>kuntarajat1000k.length-1)) {
 		       currentKunta += (currentKunta.length > 0 ? ', ' : '') + kantakartatDefs[i][0];
                ret = true;  
 		   }
