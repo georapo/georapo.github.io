a989d16125caeff5d070823a95521db060ad5029
georapo <33866355+georapo@users.noreply.github.com>
2021-06-28 18:48:24 +0300

    Update qgis2web.js

--- wanhat_gckunnat/resources/qgis2web.js
+++ wanhat_gckunnat/resources/qgis2web.js
@@ -379,13 +379,14 @@ map.on("rendercomplete", function(evt) {
 var queryString = window.location.search;    
 var urlParams = new URLSearchParams(queryString);
 var wknimi = urlParams.get('w');
+
 if (wknimi != '') {
   var gotit = false;
   
   for (var i=0; i < jsonSource_BM86ANZ_loydettywaypoints_4.getFeatures().length; i++) {
 	var wkfield = jsonSource_BM86ANZ_loydettywaypoints_4.getFeatures()[i].values_.wanhatkunnat_field_1;
     var wkn = wkfield.slice(0, wkfield.indexOf(" -"));	
-	if (wknimi.toUpperCase() == wkn.toUpperCase()) {
+	if (wkn && wknimi.toUpperCase() == wkn.toUpperCase()) {
        var wkpoint = jsonSource_BM86ANZ_loydettywaypoints_4.getFeatures()[i].values_.getGeometry; 
 	   console.log("wkpoint: "+wkpoint+" wkn:"+wkn);
 	   gotit = true;
