57e218b7cd048e615ddfa90220901638ef11602c
georapo <33866355+georapo@users.noreply.github.com>
2021-06-28 18:34:35 +0300

    urlparam w

--- wanhat_gckunnat/resources/qgis2web.js
+++ wanhat_gckunnat/resources/qgis2web.js
@@ -373,3 +373,29 @@ map.on("rendercomplete", function(evt) {
         attributionComplete = true;
     }
 })
+
+
+// jos kutsutaan kuntanimellä esim. ...&w=Ähtäri
+var queryString = window.location.search;    
+var urlParams = new URLSearchParams(queryString);
+var wknimi = urlParams.get('w');
+if (wknimi) != '' {
+
+  for (var i=0; i < jsonSource_BM86ANZ_loydettywaypoints_4.getFeatures().length; i++) {
+	var wkfield = jsonSource_BM86ANZ_loydettywaypoints_4.getFeatures()[i].values_.wanhatkunnat_field_1;
+    var wkn = wkfield.slice(0, wkfield.IndexOf(" -"));	
+	if (wknimi.toUpperCase() == wkn.toUpperCase()) {
+       var wkpoint = jsonSource_BM86ANZ_loydettywaypoints_4.getFeatures()[i].values_.getGeometry; 
+	   console.log("wkpoint: "+wkpoint);
+    }	
+  }
+	
+	
+}
+
+
+
+
+
+
+
