41784bbd90d6d6d4b49e1b3b3deb6a527ca03417
georapo <33866355+georapo@users.noreply.github.com>
2021-07-08 18:49:28 +0300

    Update BM86ANZwaypoints_3.js
    viljakkala

--- wanhat_gckunnat/layers/BM86ANZwaypoints_3.js
+++ wanhat_gckunnat/layers/BM86ANZwaypoints_3.js
@@ -15,7 +15,6 @@ var json_BM86ANZwaypoints_3 = {
 { "type": "Feature", "properties": { "wanhatkunnat_field_1": "Tenhola – liitetty 1993 Tammisaareen, joka 2009 yhdistyi Raaseporiksi" }, "geometry": { "type": "Point", "coordinates": [ 23.2962, 60.05815 ] } },
 { "type": "Feature", "properties": { "wanhatkunnat_field_1": "Uudenkaupungin mlk – liitetty 1969 Uuteenkaupunkiin" }, "geometry": { "type": "Point", "coordinates": [ 21.39175, 60.824267 ] } },
 { "type": "Feature", "properties": { "wanhatkunnat_field_1": "Ylihärmä – liitetty Kauhavaan 2009" }, "geometry": { "type": "Point", "coordinates": [ 22.79205, 63.141917 ] } },
-{ "type": "Feature", "properties": { "wanhatkunnat_field_1": "Viljakkala – liitetty Ylöjärven kaupunkiin 2007" }, "geometry": { "type": "Point", "coordinates": [ 23.2634, 61.70115 ] } },
 { "type": "Feature", "properties": { "wanhatkunnat_field_1": "Jurva – liitetty Kurikkaan 2009" }, "geometry": { "type": "Point", "coordinates": [ 21.973616666666668, 62.683466666666668 ] } },
 { "type": "Feature", "properties": { "wanhatkunnat_field_1": "Alahärmä – liitetty Kauhavaan 2009" }, "geometry": { "type": "Point", "coordinates": [ 22.854933, 63.245633 ] } },
 { "type": "Feature", "properties": { "wanhatkunnat_field_1": "Kortesjärvi – liitetty Kauhavaan 2009" }, "geometry": { "type": "Point", "coordinates": [ 23.155933, 63.299117 ] } },
